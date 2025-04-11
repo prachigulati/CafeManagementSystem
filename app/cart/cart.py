8bd7aa83afdb050b07c00c4f6efae4867f034675
prachigulati <prachigulati569@gmail.com>
2025-04-11 11:57:45 +0530

    categories added

--- app/cart/cart.py
+++ app/cart/cart.py
@@ -5,7 +5,22 @@ class Cart():
         cart = self.session.get('session_key')
         #if the user is new, no session key so create one
         if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
+            cart = self.session['cart'] = {}
 
             #make sure cart is available on all pages of site
             self.cart = cart
+
+
+    def add(self, product):
+        product_id = str(product.id)
+        #logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
+
+    
+    def __len__(self):
+        return len(self.cart)
+
--- app/cart/views.py
+++ app/cart/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404 # type: ignore
+from .cart import Cart
+from main.models import Product
+from django.http import JsonResponse # type: ignore
 # Create your views here.
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 
 def cart_add(request):
-    pass
+    #get the cart
+    cart = Cart(request)
+    #test for POST
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+        #lookup product in db
+        product = get_object_or_404(Product, id=product_id) # type: ignore
+        #save to session
+        cart.add(product=product)
+        #get cart quantity
+        # cart_quantity = cart.__len__()
+        #return response
+        response = JsonResponse({'Product Name: ': product.name})
+        # response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
