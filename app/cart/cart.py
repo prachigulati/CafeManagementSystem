c331f01aab9a4a8af9db7c46a084297bbf612f49
prachigulati <prachigulati569@gmail.com>
2025-04-12 14:20:08 +0530

    update remove ites from cart feature added

--- app/cart/cart.py
+++ app/cart/cart.py
@@ -1,26 +1,76 @@
+from main.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
         # get the current session key if it exists
-        cart = self.session.get('session_key')
+        cart = self.session.get('cart')
         #if the user is new, no session key so create one
-        if 'session_key' not in request.session:
+        if 'cart' not in request.session:
             cart = self.session['cart'] = {}
 
             #make sure cart is available on all pages of site
-            self.cart = cart
+        self.cart = cart
 
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         #logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         self.session.modified = True
 
-    
+    def cart_total(self):
+        #get product ids
+        product_ids = self.cart.keys()
+        #lookup those keys in our products databse model
+        products = Product.objects.filter(id__in = product_ids)
+        #get quantities
+        quantities = self.cart
+        #start counting at 0
+        total=0
+        for key, value in quantities.items():
+            #convert key string into int so we can do math
+            key = int(key)
+            for product in products : 
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total         
+
+
     def __len__(self):
         return len(self.cart)
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        # return those looked up products
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        #get the cart
+        ourcart = self.cart
+        ourcart[product_id]=product_qty
+        self.session.modified = True
+        thing = self.cart
+        return thing 
+    
+    def delete(self, product):
+        product_id = str(product)
+        #delete from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.session.modified =True
 
--- app/cart/views.py
+++ app/cart/views.py
@@ -5,7 +5,12 @@ from django.http import JsonResponse # type: ignore
 # Create your views here.
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    #get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 def cart_add(request):
     #get the cart
@@ -14,19 +19,49 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         #get stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         #lookup product in db
         product = get_object_or_404(Product, id=product_id) # type: ignore
         #save to session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         #get cart quantity
-        # cart_quantity = cart.__len__()
+        cart_quantity = cart.__len__()
         #return response
-        response = JsonResponse({'Product Name: ': product.name})
-        # response = JsonResponse({'qty': cart_quantity})
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
         return response
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+        #call delete function in cart
+        cart.delete(product=product_id)
+        response = JsonResponse({'product':product_id})
+        return response
+
+
+# def cart_update(request):
+#     cart = Cart(request)
+#     if request.POST.get('action') == 'post':
+#         #get stuff
+#         product_id = int(request.POST.get('product_id'))
+#         product_qty = int(request.POST.get('product_qty'))
+#         cart.update(product=product_id, quantity=product_qty)
+#         response = JsonResponse({'qty':product_qty})
+#         return response
+
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        product_qty = request.POST.get('product_qty')
+        if product_id and product_qty and product_qty.isdigit():
+            product_id = int(product_id)
+            product_qty = int(product_qty)
+            cart.update(product=product_id, quantity=product_qty)
+            return JsonResponse({'qty': product_qty})
+        else:
+            return JsonResponse({'error': 'Invalid product ID or quantity'}, status=400)
